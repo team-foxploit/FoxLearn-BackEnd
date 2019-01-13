ebc647c90a6b0afc0a8bb25cae61bc7b2cd014ff
luk3Sky <ganindudananja@gmail.com>
2019-01-13 11:32:36 +0530

    Add cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const cors = require('cors');
 
 // Express app
 const app = express();
+
+app.use(cors());
 
 app.use(bodyparser.json());
 
