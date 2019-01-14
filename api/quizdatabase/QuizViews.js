533ef17556df7b345fdac1765c1a959075144140
luk3Sky <ganindudananja@gmail.com>
2019-01-15 00:29:58 +0530

    Update QuizViews.js

--- api/quizdatabase/QuizViews.js
+++ api/quizdatabase/QuizViews.js
@@ -1,6 +1,18 @@
-// Get all usernames from both teachers and students
+// Get one question, associated answers and correct answer
 module.exports.getOneQuestion = function (connection, req, res) {
     let que = `CALL get_Quiz('${req.body.Q_ID}', '${req.body.Subject}', '${req.body.Difficulty}');`;
+    connection.query(que, (error, result, fields) => {
+        if(error){
+            res.sendStatus(400);
+            return console.error(error.message);
+        }
+        res.json(result);
+    } );
+};
+
+// Get complete quiz with answers and correct answers
+module.exports.getCompleteQuiz = function (connection, req, res) {
+    let que = `CALL get_ques('${req.body.Q_ID1}', '${req.body.Q_ID2}', '${req.body.Q_ID3}', '${req.body.Q_ID4}', '${req.body.Q_ID5}');`;
     connection.query(que, (error, result, fields) => {
         if(error){
             res.sendStatus(400);
