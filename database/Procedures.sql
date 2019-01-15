e66ebe7cec24f5ff3f67e1cccd5648cf2ede1be9
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2019-01-15 21:01:41 +0530

    Update Procedures.sql

--- database/Procedures.sql
+++ database/Procedures.sql
@@ -27,16 +27,16 @@ DELIMITER ;
 
 //--------------------------Insert procedure for questions -----------------------------------------
 
-DELIMITER $$
-      CREATE PROCEDURE insert_ques_ans(IN Question_Id_p VARCHAR(255),IN Correct_Answer_Id_p INT,IN Question_p varchar(500),IN Subject_Id_p INT,IN Teacher_Id_p INT, IN Answer_1_p VARCHAR(45), IN Answer_2_p VARCHAR(45), IN Answer_3_p VARCHAR(45), IN Answer_4_p VARCHAR(45))
-        BEGIN
-        INSERT INTO Question (Ques_ID,Crct_Ans_Id,Question,Sub_ID,Tch_ID) VALUES (Question_Id_p,Correct_Answer_Id_p,Question_p,Subject_Id_p,Teacher_Id_p);
-	INSERT INTO Answer ( Ans_ID, Answer,Ques_ID) VALUES ( 1, Answer_1_p, Question_Id_p);
-	INSERT INTO Answer ( Ans_ID, Answer,Ques_ID) VALUES ( 2, Answer_2_p, Question_Id_p);
-	INSERT INTO Answer ( Ans_ID, Answer,Ques_ID) VALUES ( 3, Answer_3_p, Question_Id_p);
-    INSERT INTO Answer ( Ans_ID, Answer,Ques_ID) VALUES ( 4, Answer_4_p, Question_Id_p);
-	END$$
-DELIMITER ;
+-- DELIMITER $$
+--       CREATE PROCEDURE insert_ques_ans(IN Question_Id_p VARCHAR(255),IN Correct_Answer_Id_p INT,IN Question_p varchar(500),IN Subject_Id_p INT,IN Teacher_Id_p INT, IN Answer_1_p VARCHAR(45), IN Answer_2_p VARCHAR(45), IN Answer_3_p VARCHAR(45), IN Answer_4_p VARCHAR(45))
+--       BEGIN
+--       INSERT INTO Question (Ques_ID,Crct_Ans_Id,Question,Sub_ID,Tch_ID) VALUES (Question_Id_p,Correct_Answer_Id_p,Question_p,Subject_Id_p,Teacher_Id_p);
+-- 	    INSERT INTO Answer ( Ans_ID, Answer,Ques_ID) VALUES ( 1, Answer_1_p, Question_Id_p);
+-- 	    INSERT INTO Answer ( Ans_ID, Answer,Ques_ID) VALUES ( 2, Answer_2_p, Question_Id_p);
+-- 	    INSERT INTO Answer ( Ans_ID, Answer,Ques_ID) VALUES ( 3, Answer_3_p, Question_Id_p);
+--       INSERT INTO Answer ( Ans_ID, Answer,Ques_ID) VALUES ( 4, Answer_4_p, Question_Id_p);
+-- 	    END$$
+-- DELIMITER ;
 
 
 
@@ -46,7 +46,7 @@ DELIMITER ;
 //---------------------Retrieve the Question , answers and the correct answer---------------------------------------------------
 
 DELIMITER $$
-      CREATE PROCEDURE get_Quiz(IN Question_Id_p varchar(255),IN Subject_Name_p varchar(45),IN Difficulty_p VARCHAR(10))
+      CREATE PROCEDURE get_Quiz(IN Question_Id_p INT, IN Subject_Name_p varchar(45), IN Difficulty_p VARCHAR(10))
         BEGIN
         SELECT Question.Question ,Question.Crct_Ans_Id, Question.Difficulty, Subject.Subject_Name, Answer.Ans_ID, Answer.Answer FROM Question,Answer,Subject WHERE Question_Id_p = Question.Ques_ID AND Question_Id_p = Answer.Ques_ID AND Subject.Subject_Name = Subject_Name_p AND Question.Difficulty = Difficulty_p;
         END$$
@@ -86,12 +86,50 @@ DELIMITER ;
 //---------------------Retrieve 5 Questions , answers and the correct answers---------------------------------------------------
 
 DELIMITER $$
-      CREATE PROCEDURE get_ques(IN Question_Id_1 varchar(255),IN Question_Id_2 varchar(255),IN Question_Id_3 varchar(255),IN Question_Id_4 varchar(255),IN Question_Id_5 varchar(255))
+      CREATE PROCEDURE get_ques(IN Question_Id_1 INT,IN Question_Id_2 INT,IN Question_Id_3 INT,IN Question_Id_4 INT,IN Question_Id_5 INT)
         BEGIN
         SELECT Question.Question ,Question.Crct_Ans_Id,Answer.Ans_ID, Answer.Answer FROM Question,Answer WHERE Question_Id_1 = Question.Ques_ID AND Question_Id_1= Answer.Ques_ID;
-	SELECT Question.Question ,Question.Crct_Ans_Id,Answer.Ans_ID, Answer.Answer FROM Question,Answer WHERE Question_Id_2 = Question.Ques_ID AND Question_Id_2= Answer.Ques_ID;
-	SELECT Question.Question ,Question.Crct_Ans_Id,Answer.Ans_ID, Answer.Answer FROM Question,Answer WHERE Question_Id_3 = Question.Ques_ID AND Question_Id_3= Answer.Ques_ID;
-	SELECT Question.Question ,Question.Crct_Ans_Id,Answer.Ans_ID, Answer.Answer FROM Question,Answer WHERE Question_Id_4 = Question.Ques_ID AND Question_Id_4= Answer.Ques_ID;
-	SELECT Question.Question ,Question.Crct_Ans_Id,Answer.Ans_ID, Answer.Answer FROM Question,Answer WHERE Question_Id_5 = Question.Ques_ID AND Question_Id_5= Answer.Ques_ID;
+	      SELECT Question.Question ,Question.Crct_Ans_Id,Answer.Ans_ID, Answer.Answer FROM Question,Answer WHERE Question_Id_2 = Question.Ques_ID AND Question_Id_2= Answer.Ques_ID;
+	      SELECT Question.Question ,Question.Crct_Ans_Id,Answer.Ans_ID, Answer.Answer FROM Question,Answer WHERE Question_Id_3 = Question.Ques_ID AND Question_Id_3= Answer.Ques_ID;
+	      SELECT Question.Question ,Question.Crct_Ans_Id,Answer.Ans_ID, Answer.Answer FROM Question,Answer WHERE Question_Id_4 = Question.Ques_ID AND Question_Id_4= Answer.Ques_ID;
+	      SELECT Question.Question ,Question.Crct_Ans_Id,Answer.Ans_ID, Answer.Answer FROM Question,Answer WHERE Question_Id_5 = Question.Ques_ID AND Question_Id_5= Answer.Ques_ID;
         END$$
+DELIMITER ;
+
+//------------------------------Insert Question into Question table--------------------------------------------
+DELIMITER //
+CREATE PROCEDURE submitQuiz(
+  IN correctAnsID INT,
+  IN question VARCHAR(500),
+  IN subID INT,
+  IN teacherID INT,
+  IN difficulty VARCHAR(10),
+  IN ans1 VARCHAR(45),
+  IN ans2 VARCHAR(45),
+  IN ans3 VARCHAR(45),
+  IN ans4 VARCHAR(45))
+
+  BEGIN
+    DECLARE x  INT;
+    DECLARE str  VARCHAR(10);
+    DECLARE y INT;
+
+    SET x=1;
+    DROP TABLE IF EXISTS ans_table;
+    CREATE TEMPORARY TABLE ans_table (ans_id INT, ans VARCHAR(45));
+    INSERT INTO ans_table (ans_id, ans) VALUES (1,ans1),(2,ans2),(3,ans3),(4,ans4);
+
+    INSERT INTO Question(Crct_Ans_Id, Question, Sub_ID, Tch_ID, Difficulty)
+    VALUES (correctAnsID, question, subID, teacherID, difficulty);
+
+    SET y=LAST_INSERT_ID();
+
+    WHILE x<=4 DO
+      INSERT INTO Answer(Ans_ID,Answer,Ques_ID)
+      VALUES (x,(SELECT ans FROM ans_table WHERE ans_id = x),y);
+      SET  x = x + 1;
+    END WHILE;
+
+  END //
+
 DELIMITER ;
