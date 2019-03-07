5c1f0b486acccf437f2f96fb17ee7fcb1ad87e51
luk3Sky <ganindudananja@gmail.com>
2019-03-08 01:56:06 +0530

    Update demo_test.js

--- test/demo_test.js
+++ test/demo_test.js
@@ -1,8 +1,7 @@
-const mocha = require('mocha');
 const assert = require('assert');
 
 // Describe some test
-describe('some demo test', () =>{
+describe('some demo test', function(){
     // Create test
     it('add two numbers', () => {
         assert(2+3 === 5);
