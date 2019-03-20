cd651b8030ebc092bc35a9a5b1beaaa37e32014c
luk3Sky <ganindudananja@gmail.com>
2019-03-20 21:12:01 +0530

    Update UserRoutes.js

--- api/routes/UserRoutes.js
+++ api/routes/UserRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
-const User = require('../models/Users');
+const User = require('../models/User');
 
 // Get all users
 router.get('/', (req, res, next) => {
