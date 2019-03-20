d6cd747d174ed5cfa279c2bbe4b750b195cc258d
luk3Sky <ganindudananja@gmail.com>
2019-03-20 21:11:50 +0530

    User model updated

--- api/models/User.js
+++ api/models/User.js
@@ -21,6 +21,10 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
+    password: {
+        type: String,
+        required: true
+    },
     userType: {
         type: String,
         required: true
